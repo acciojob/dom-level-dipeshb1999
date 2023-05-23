384bbef18184a1c1c09810efd2e4682ba13f672f
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-23 18:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Find the DOM level of an element by its ID
+function findDOMLevelById(elementId) {
+    var element = document.getElementById(elementId);
+    var level = 0;
+
+    while (element.parentNode) {
+        level++;
+        element = element.parentNode;
+    }
+
+    return level;
+}
+
+// Call the function and display the result using alert()
+var levelElementId = "level";
+var level = findDOMLevelById(levelElementId);
+alert("The level of the element is: " + level);
